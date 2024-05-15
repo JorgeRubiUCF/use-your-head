d765e2eae6b74045ea72a42bbb836e0f4b57f330
melic <flafishing@gmail.com>
2024-05-15 00:36:29 -0400

    fixed some spritesheets, added hold head, changed brick to purble, working on camera tweaaks

---
+++ Camera2D.gd
@@ -0,0 +1,31 @@
+extends Camera2D
+
+var zoomspeed = 50
+var zoommargin = 0.3
+
+var zoomMin = 1.5
+var zoomMax = 2
+
+var zoompos = Vector2()
+var zoomfactor = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	zoom.x = lerp(zoom.x, zoom.x * zoomfactor, zoomspeed * delta)
+	zoom.y = lerp(zoom.y, zoom.y * zoomfactor, zoomspeed * delta)
+	
+	zoom.x = clamp(zoom.x, zoomMin, zoomMax)
+	zoom.y = clamp(zoom.y, zoomMin, zoomMax)
+	
+	zoomfactor = clamp(zoomfactor, 0, 1)
+	
+	if Input.is_action_pressed("hold"):
+		zoomfactor -= 0.01
+	else:
+		zoomfactor += 0.01
+		
